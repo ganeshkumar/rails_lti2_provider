52cd2e5321d336775f42b38254de35f26541a440
ganesh <ganesh.dc@gmail.com>
2021-03-30 14:37:24 +0530

    removed correlation_id causing issue

--- db/migrate/20151204000125_add_rails_lti2_provider_registration_correlation_id.rb
+++ db/migrate/20151204000125_add_rails_lti2_provider_registration_correlation_id.rb
@@ -1,6 +1,5 @@
 class AddRailsLti2ProviderRegistrationCorrelationId < ActiveRecord::Migration
   def change
     add_column :rails_lti2_provider_registrations, :correlation_id, :text
-    add_index :rails_lti2_provider_registrations, :correlation_id, :unique => true
   end
 end
